3690bc609c82aefbdc88d93d9a41519fa95c5420
Emory <aa774281@gmail.com>
2022-02-23 14:13:57 +0800

    add tasks of webp-convert and build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 // 引入需要的模組
-const { gulp, task, series, src, dest, watch } = require("gulp");
+const { gulp, task, series, src, dest, watch, parallel } = require("gulp");
 const autoprefixer = require("autoprefixer");
 const postcss = require("gulp-postcss");
 const sourcemaps = require("gulp-sourcemaps");
 const cssimport = require("gulp-cssimport");
 const cleancss = require("gulp-clean-css");
 const concat = require("gulp-concat");
+const webp = require("gulp-webp");
 
 // 打包所有 CSS 程式碼，並使用 autoprefixer 提升 css 在不同瀏覽器的兼容性
 function BundleCSS(cb) {
@@ -25,7 +26,19 @@ function WatchCSS() {
   watch("src/css/*.css", BundleCSS);
 }
 
+// 將所有 .jpg, .png 檔案轉換成 .webp
+function ConvertToWebP(cb) {
+  src("public/img/ori/*.{jpg,png}")
+    .pipe(webp({ quality: 70 }))
+    .pipe(dest("public/img/webp"));
+  cb();
+}
+
+// 產生最終檔案內容，包括打包 css，轉換 .jpg, .png 成 .webp 檔案
+var Build = parallel(BundleCSS, ConvertToWebP);
+
 // 發包任務
-exports.default = BundleCSS;
-exports.build = BundleCSS;
+exports.default = Build;
+exports.build = Build;
 exports.dev = WatchCSS;
+exports.webp = ConvertToWebP;
